2ec8e4497da6c4fa75a1867f521b0b11875ed71b
hanchaeyoung <gks4167@naver.com>
2023-09-19 12:17:47 +0900

    230919 - 프로젝트 생성

---
+++ naverblog_clonecoding/src/main/java/spring/naverblog_clonecoding/NaverblogClonecodingApplication.java
@@ -0,0 +1,14 @@
+package spring.naverblog_clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverblogClonecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverblogClonecodingApplication.class, args);
+		System.out.println("실행완료");
+	}
+
+}
---
+++ naverblog_clonecoding/src/test/java/spring/naverblog_clonecoding/NaverblogClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.naverblog_clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverblogClonecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
